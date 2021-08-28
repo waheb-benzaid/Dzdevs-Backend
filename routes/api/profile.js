830c6d96e90f08bac42582d029fc1c149fecee4a
waheb-benzaid <waheb.benzaid@gmail.com>
2021-08-28 01:48:48 +0100

    create and update profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const auth = require("../../middleware/auth")
 const Profile = require("../../models/Profile")
 const User = require("../../models/User")
+const { check, validationResult } = require("express-validator/check")
 
 // @route GET api/profile/me
 // @desc GET current users profile route
@@ -24,5 +25,104 @@ router.get("/me", auth, async (req, res) => {
     res.status(500).send("server error")
   }
 })
+
+// @route POST api/profile
+// @desc create & update users profile route
+// @access Private
+
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("status", "status is required").not().isEmpty(),
+      check("skills", "Skills are required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    console.log(req.body)
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() }) //this test means that there are some errors (Bad request)
+    }
+
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      twitter,
+      instagram,
+      linkedin,
+      facebook,
+    } = req.body
+
+    //Build profile object
+    const profileFields = {}
+    profileFields.user = req.user.id
+    if (company) {
+      profileFields.company = company
+    }
+    if (website) {
+      profileFields.website = website
+    }
+    if (location) {
+      profileFields.location = location
+    }
+    if (bio) {
+      profileFields.bio = bio
+    }
+    if (status) {
+      profileFields.status = status
+    }
+    if (githubusername) {
+      profileFields.githubusername = githubusername
+    }
+    if (skills) {
+      profileFields.skills = skills.split(",").map((skill) => skill.trim())
+    }
+
+    //Build Social object
+    profileFields.social = {}
+    if (youtube) {
+      profileFields.social.youtube = youtube
+    }
+    if (twitter) {
+      profileFields.social.twitter = twitter
+    }
+    if (instagram) {
+      profileFields.social.instagram = instagram
+    }
+    if (facebook) {
+      profileFields.social.facebook = facebook
+    }
+    if (linkedin) {
+      profileFields.social.linkedin = linkedin
+    }
+    try {
+      let profile = await Profile.findOne({ user: req.user.id })
+      if (profile) {
+        //if profile exists, the update mode is activated, else I will insert the profile because it does not exixts
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true },
+        )
+        return res.json(profile)
+      }
+      //Mode insert activated if the profile does not exist
+      profile = new Profile(profileFields)
+      await profile.save()
+      return res.json(profile)
+    } catch (err) {
+      console.log(err.message)
+      res.status(500).send("server error")
+    }
+  },
+)
 
 module.exports = router
