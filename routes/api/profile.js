cd2ae02d8a80299c928346a0bfd272e5a4d04382
waheb-benzaid <waheb.benzaid@gmail.com>
2021-09-02 16:49:47 +0100

    Add the put router of profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -181,4 +181,50 @@ router.delete("/", auth, async (req, res) => {
     res.status(500).send("server error")
   }
 })
+
+// @route PUT api/profile/experience
+// @desc  Add profile experience
+// @access private
+router.put(
+  "/experience",
+  [
+    auth,
+    [
+      check("title", "Title is required").not().isEmpty(),
+      check("company", "Company is required").not().isEmpty(),
+      check("from", "From date is required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+
+    const { title, company, location, from, to, current, description } =
+      req.body
+
+    const newExperience = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    }
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id })
+      console.log(profile)
+
+      profile.experience.unshift(newExperience)
+      await profile.save()
+      res.json(profile)
+    } catch (err) {
+      console.log(err.message)
+      res.status(500).send("server error")
+    }
+  },
+)
 module.exports = router
