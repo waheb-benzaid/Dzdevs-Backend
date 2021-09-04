a879de244b4123972391a05c8a97bd5ace15e74a
waheb-benzaid <waheb.benzaid@gmail.com>
2021-09-04 02:51:18 +0100

    get post by postId

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -49,4 +49,23 @@ router.get("/", auth, async (req, res) => {
   }
 })
 
+// @route GET api/posts/:id
+// @desc  Get post by id
+// @access Private
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+    if (!post) {
+      return res.status(404).json({ msg: "post is not found" })
+    }
+    res.json(post)
+  } catch (err) {
+    console.error(err.message)
+    if (err.kind === "objectId") {
+      return res.status(404).json({ msg: "post is not found" })
+    }
+    res.status(500).send("server error")
+  }
+})
+
 module.exports = router
